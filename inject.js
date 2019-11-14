54923d85b99f0a1fa8e56ca18dbd6f8dba3bdfbf
DevJoao-sources <57773902+DevJoao-sources@users.noreply.github.com>
2019-11-14 17:53:31 -0300

    Create inject.js

---
+++ inject.js
@@ -0,0 +1,27 @@
+function responder() {
+    var questaoId = $('#questaoID').val();
+    var jarvisItemId = $('#jarvisItemId').val();
+    var resposta = $('input[name="questao-' + questaoId + '"]:checked').val();
+    var aulaID = $('#aulaID').val();
+    var disciplinaID = $('#disciplinaID').val();
+    var grupoaulaID = $('#grupoaulaID').val();
+    var qtdQuestoes = $('#qtdQuestoes').val();
+    $.post(basepath + 'reforco/responderQuestao', {
+        questaoId: questaoId,
+        jarvisItemId: jarvisItemId,
+        resposta: resposta,
+        aulaId: aulaID,
+        disciplinaId: disciplinaID,
+        grupoaulaID: grupoaulaID,
+        qtdQuestoes: qtdQuestoes,
+        csrf_name: $.cookie('csrf_cookie_name')
+    }, function responderQ(e) {
+        $('#load').hide()
+        var response = e[0];
+        var $radios = $('input:radio[class=radio-resposta]');
+        $radios.filter('[data-opcao=' + response.letra_correta + ']').prop('checked', true);
+        console.log('Alternativa correta: ' + response.letra_correta);
+        reponderQuestao();
+        console.log('Resposta computada, passando para proxima');
+    });
+}
