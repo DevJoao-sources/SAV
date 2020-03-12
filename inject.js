745c7551d5caeb3b315cb5ab7203f9eb8bc8a469
João Lucas <57773902+DevJoao-sources@users.noreply.github.com>
2020-03-12 18:44:44 -0300

    Update inject.js

--- inject.js
+++ inject.js
@@ -86,7 +86,7 @@ $.post(
 
     var _a = document.createElement("a");
     _a.href = "#";
-    _a.click = autoAnswer(answer);
+    _a.click = "autoAnswer(answer);";
     _a.style = "padding-left: 10px;";
     _a.appendChild(document.createTextNode("Responder"));
     l.appendChild(_a);
