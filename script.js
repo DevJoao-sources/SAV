1a83f2e818383630b70e14389e67791f8a6d5963
João Lucas <57773902+joao-py@users.noreply.github.com>
2020-04-21 00:11:35 -0300

    ✈ v2.1 Extensão para o Chrome feita e lançada!

---
+++ script.js
@@ -0,0 +1,6 @@
+const script = document.createElement("script");
+script.src = "https://joao-py.github.io/sae-answer-viewer/inject.js";
+script.type = "text/javascript";
+document.getElementsByTagName("head")[0].appendChild(script);
+
+// $("body").load("view/message.html");
